79b9b86759703b2eb42658ead7beef322a1bfc69
JeffT <tdhuy2001@gmail.com>
2024-01-23 01:46:37 -0500

    first step in MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { getHomePage } = require('../controllers/homeControllers')
 
-router.get('/', (req, res) => {
-    res.render('sample')
-})
+router.get('/', getHomePage)
 
 module.exports = router
