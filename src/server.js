7ca5f2380d7a20a947baf79aa309c687e87bab7a
JeffT <tdhuy2001@gmail.com>
2024-01-23 01:06:39 -0500

    seprate the route

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 const express = require("express")
 const path = require('path')
 require('dotenv').config()
+const configViewEngine = require('./config/viewEngine')
+const webRoutes = require('./routes/web')
 
 const app = express()
 const port = process.env.PORT || 8081
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// config route
+app.use('/', webRoutes)
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.render('sample')
-})
+// config view template
+configViewEngine(app)
 
 app.listen(port, () => {
     console.log(`litsening on port ${port}`)
