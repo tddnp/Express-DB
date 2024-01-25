a470e0e82c6413b10b562d26a3655b8aebbac7d5
JeffT <tdhuy2001@gmail.com>
2024-01-25 05:51:34 -0500

    connect to db

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path')
 require('dotenv').config()
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2')
 
 const app = express()
 const port = process.env.PORT || 8081
@@ -12,6 +13,23 @@ app.use('/', webRoutes)
 
 // config view template
 configViewEngine(app)
+
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'express'
+})
+
+connection.query(
+    'SELECT * FROM Users',
+    function (err, results, field) {
+        console.log('results = ', results)
+        console.log('field = ', field)
+    }
+)
 
 app.listen(port, () => {
     console.log(`litsening on port ${port}`)
